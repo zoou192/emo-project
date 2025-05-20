8f50999d1b339f4349034c9d6fcb1e7d2655c835
wb_changyanchun <wb_changyanchun@meituan.com>
2025-05-20 13:42:36 +0800

    feat: fix

---
+++ dist/assets/pinia-64d28fb8.js
@@ -0,0 +1,8 @@
+import{e as t,b as a,m as s}from"./vue-1c2f1dbf.js";
+/*!
+ * pinia v2.3.1
+ * (c) 2025 Eduardo San Martin Morote
+ * @license MIT
+ */
+const c=Symbol();var e,n;function o(){const e=t(!0),n=e.run((()=>a({})));let o=[],r=[];const i=s({install(t){i._a=t,t.provide(c,i),t.config.globalProperties.$pinia=i,r.forEach((t=>o.push(t))),r=[]},use(t){return this._a?o.push(t):r.push(t),this},_p:o,_a:null,_e:e,_s:new Map,state:n});return i}(n=e||(e={})).direct="direct",n.patchObject="patch object",n.patchFunction="patch function";export{o as c};
+//# sourceMappingURL=pinia-64d28fb8.js.map
