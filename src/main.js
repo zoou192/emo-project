644a53839e11e48de21ee7ff42594c97efd75d02
wb_changyanchun <wb_changyanchun@meituan.com>
2025-05-20 15:47:08 +0800

    feat: fix

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import '@/assets/styles/main.scss'
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app') 
